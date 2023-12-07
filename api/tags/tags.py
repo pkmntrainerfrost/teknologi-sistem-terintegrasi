88fb39d2bc67ab8d1ce83afd5c86c4dd37779830
pkmntrainerfrost <smaarif.achsien@gmail.com>
2023-12-07 12:53:34 +0700

    initial commit

--- api/tags/tags.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-from ..votes import votes
-
-class AppliedTag(BaseModel):
-	tag_name: str
-	votes: List[votes.Vote]
-	community: bool
--- api/votes/votes.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from pydantic import BaseModel
-
-class Vote(BaseModel):
-    
-    user_id : int
-    positive : bool
